38e650486fc667f51b2aeefad3e654d2eb1987ae
frontuser7 <frontuser7@qr4order.com>
2024-01-21 14:24:48 +0530

    new changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { BsCartFill } from "react-icons/bs";
 
 function Navbar() {
   return (
-    <div className="flex justify-center p-3 sticky top-0 bg-white z-10">
-      <div className="text-red text-2xl font-logo">UniqueFinds</div>
-      <div className="absolute right-0 flex pr-3">
-        <BsCartFill className="text-primary" />
+    <div className="text-center p-4">
+      <div className="text-2xl font-medium text-red font-logo">
+        HungerCraker
       </div>
     </div>
   );
--- src/herosection/Dashboard.jsx
+++ src/herosection/Dashboard.jsx
@@ -1,16 +1,7 @@
 import React from "react";
-import Categories from "../components/Categories";
-import ProductList from "./ProductList";
-import BestChoice from "../components/BestChoice";
 
 function Dashboard() {
-  return (
-    <div className="mx-6">
-      <BestChoice />
-      <Categories />
-      <ProductList />
-    </div>
-  );
+  return <div className="mx-6">hi</div>;
 }
 
 export default Dashboard;

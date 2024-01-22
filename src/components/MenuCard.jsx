e63e37e184870879b2720fb25aaf83379c1af7d8
AjinkyaVeer007 <veerajinkya1@gmail.com>
2024-01-22 18:08:57 +0530

    image bug fixed

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -3,11 +3,19 @@ import React from "react";
 function MenuCard({ data }) {
   return (
     <div className="rounded-lg border border-border relative">
-      <img
-        className="rounded w-full h-[150px] aspect-video object-cover object-top"
-        src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/${data?.imageId}`}
-        alt="img"
-      />
+      {data?.imageId ? (
+        <img
+          className="rounded w-full h-[150px] aspect-video object-cover object-top"
+          src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/${data?.imageId}`}
+          alt="img"
+        />
+      ) : (
+        <img
+          className="rounded w-full h-[150px] aspect-video object-cover object-top"
+          src="https://www.survivorsuk.org/wp-content/uploads/2017/01/no-image.jpg"
+          alt="img"
+        />
+      )}
       <div className="p-4">
         <div className="mb-6 font-medium text-[12px] truncate">
           {data?.name}

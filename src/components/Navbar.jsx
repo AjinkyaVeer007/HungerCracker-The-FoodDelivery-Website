9eebcf61d034b202f15a2e0016272df11130e7ec
AjinkyaVeer007 <frontuser7@qr4order.com>
2024-01-22 12:37:49 +0530

    demo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 function Navbar() {
   const navigate = useNavigate();
   return (
-    <div className="text-center p-4 sticky top-0 bg-white z-10">
+    <div className="text-center p-4 sticky top-0 bg-white z-10 ">
       <div
         onClick={() => navigate("/")}
         className="text-4xl font-medium text-red font-logo cursor-pointer"
